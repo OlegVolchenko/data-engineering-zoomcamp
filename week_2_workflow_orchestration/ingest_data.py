34735d9ac77c1396de323f678a6dbb87facbd801
Oleg Volchenko <oleg.volchenko@ingka.ikea.com>
2023-01-22 22:49:53 +0100

    add a connection block

--- week_2_workflow_orchestration/ingest_data.py
+++ week_2_workflow_orchestration/ingest_data.py
@@ -3,14 +3,15 @@
 
 import os
 
-from time import time
+from datetime import timedelta
+from prefect_sqlalchemy import SqlAlchemyConnector
 
 import pandas as pd
 from prefect import flow, task
-from sqlalchemy import create_engine
+from prefect.tasks import task_input_hash
 
 
-@task(log_prints=True, retries=3, name='get data')
+@task(log_prints=True, retries=3, name='get data', cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
 def get_data(url: str) -> pd.DataFrame:
     """Get csv data and transform to pd.DataFrame"""
     print("Reading a csv file")
@@ -24,9 +25,9 @@ def transform_data(df: pd.DataFrame) -> pd.DataFrame:
     """Cleans and prepare a datframe"""
     # remove line with missing VendorId
     print('transforming a df')
-    print(f'Cleaning missing vendor rows, amount of affected rows: {df[df["VendorID"].isnull()]}')
+    print(f'Cleaning missing vendor rows, amount of affected rows: {len(df[df["VendorID"].isnull()])}')
     df = df[~df["VendorID"].isnull()]
-    print(f'Cleaning invalid passenger count, amount of affected rows: {df[df["passenger_count"] <= 0]}')
+    print(f'Cleaning invalid passenger count, amount of affected rows: {len(df[df["passenger_count"] <= 0])}')
     # remove lines with invalid passenger count
     df = df[df["passenger_count"] > 0]
     print('Converting columns to datetime dtype')
@@ -37,19 +38,18 @@ def transform_data(df: pd.DataFrame) -> pd.DataFrame:
 
 
 @task(log_prints=True, name='ingest date to sql')
-def load(df, table_name: str, db: str)-> None:
-    print('creating connection to postgres')
-    engine = create_engine(f'postgresql://root:root@localhost:5432/{db}')
-    print('ingesting data ...')
-    df.to_sql(name=table_name, con=engine, if_exists='append')
-    print('ingestion is complete')
+def load(df, table_name: str) -> None:
+    with SqlAlchemyConnector.load("postgres-taxi-db").get_connection(begin=False) as engine:
+        print('Ingesting df to postgres')
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+        print('ingestion is complete')
 
 
 @flow(log_prints=True, name='ingest flow')
-def ingest_data(url: str, db: str = 'ny_taxi', table_name: str = 'yellow_taxi_trips') -> None:
+def ingest_data(url: str, table_name: str = 'yellow_taxi_trips') -> None:
     df = get_data(url)
     df = transform_data(df)
-    load(df, table_name, db)
+    load(df, table_name)
 
 
 if __name__ == '__main__':
